3961bcdc950c552bb0cbfb8a49a7066423399f84
Rishad Alam <rishad.s.a@gmail.com>
2023-06-13 10:24:53 -0400

    complete

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-function sumItems(array) {
+const sumItems = (array) => {
   // Sum all the numbers in the array
-}
+  //process input so that it is flat
+  let sum = 0;
+  const flatArray = (array) => {
+    //Iterate through array
+    for (let item of array) {
+      // Identify if index is an array
+      if (Array.isArray(item)) {
+        flatArray(item); //recursively call function
+      } else {
+        sum += item; //add to sum
+      }
+    }
+    return sum;
+  };
+  return flatArray(array);
+};
 
 module.exports = sumItems;
